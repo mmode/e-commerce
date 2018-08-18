b190431da68b330b145c288c3c3d789946f2b8a5
Nicola D'Aniello <nicola.danie@gmail.com>
2018-08-18 12:39:59 +0100

    add firebase & angularfire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBEWUCiiUljFHhxEq-2csAnrUieN3MdUeo',
+    authDomain: 'e-commerce-c2086.firebaseapp.com',
+    databaseURL: 'https://e-commerce-c2086.firebaseio.com',
+    projectId: 'e-commerce-c2086',
+    storageBucket: 'e-commerce-c2086.appspot.com',
+    messagingSenderId: '88886680487'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBEWUCiiUljFHhxEq-2csAnrUieN3MdUeo',
+    authDomain: 'e-commerce-c2086.firebaseapp.com',
+    databaseURL: 'https://e-commerce-c2086.firebaseio.com',
+    projectId: 'e-commerce-c2086',
+    storageBucket: 'e-commerce-c2086.appspot.com',
+    messagingSenderId: '88886680487'
+  }
 };
 
 /*
