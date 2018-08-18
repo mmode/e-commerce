ed85f997c744e9e88a55d20f95d6d7e0e9c364d7
Nicola D'Aniello <nicola.danie@gmail.com>
2018-08-18 15:06:16 +0100

    implement login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afs: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.afs.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
 }
