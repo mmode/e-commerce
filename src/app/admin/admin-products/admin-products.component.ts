5ad8005a4b26dca65ee1085f7813d946138e6898
Nicola D'Aniello <nicola.danie@gmail.com>
2018-08-22 13:50:04 +0100

    implement admin products filter

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,21 +1,33 @@
 import { ProductService } from './../../services/product.service';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
-  products$: Observable<any>;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products = [];
+  filteredProducts = [];
+  subscription: Subscription;
 
   constructor(
     private productService: ProductService
   ) { }
 
   ngOnInit() {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query)
+      ? this.filteredProducts.filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
+      : this.products;
+  }
 }
