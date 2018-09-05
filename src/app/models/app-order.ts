fcd0b78e7b3e618eed71df3602e1096a05473f38
Nicola D'Aniello <nicola.danie@gmail.com>
2018-09-05 18:43:57 +0100

    bugfix:  hide shoppingCart in order class

--- src/app/models/app-order.ts
+++ src/app/models/app-order.ts
@@ -7,10 +7,10 @@ export class AppOrder {
     constructor(
         public userId: string,
         public shipping: any,
-        private shoppingCart: ShoppingCart
+        shoppingCart: ShoppingCart
     ) {
         this.datePlaced = new Date().getTime();
-        this.items = this.shoppingCart.items.map(i => {
+        this.items = shoppingCart.items.map(i => {
             return {
               product: {
                 title: i.title,
